7f03df5de73567dddf2b435d2fcdd742b7556de2
STDSuperman <2750556766@qq.com>
2021-12-18 15:41:17 +0800

    :sparkles: feat: 支持使用 localforage 缓存方案
    ISSUES CLOSED: +

--- packages/web-torrent-render/src/worker.ts
+++ packages/web-torrent-render/src/worker.ts
@@ -25,65 +25,34 @@ self.addEventListener('activate', async event => {
   logger.info('activated!')
 })
 
-// self.addEventListener('fetch', async event => {
-//   const request = event.request;
-//   const scope = self.registration.scope;
-//   const method = event.request.method;
-//   const url = request.url;
-//   const cacheDB = await caches.open(CACHE_NAME);
-
-//   if (method.toUpperCase() !== 'GET' || url.indexOf(scope) !== 0) return;
-
-//   const fetchPath = url.slice(scope?.length ?? 0);
-//   logger.info('Current fetch request is', fetchPath);
-//   console.log(event);
-
-//   if (!fetchPath) {
-//     logger.log('enter fetchPath')
-//     event.respondWith(self.fetch('/index.html'));
-//   } else if (fetchPath.indexOf('intercept/status') !== -1) {
-//     logger.log('enter intercept/status')
-//     // @ts-ignore
-//     event.respondWith(new self.Response('', { status: 234, statusText: 'intercepting' }))
-//   } else {
-//     console.log('xxx下来', await fetch(request))
-//     // logger.info(`current storage cache keys: ${await localforage.keys()}`);
-//     // const currentFileBlob: string = await localforage.getItem(`${staticPrefix}/${fetchPath}`)
-//     // event.respondWith(handlerFetch(currentFileBlob, event));
-//     // event.respondWith(response)
-
-//     // console.log('cache db', await cacheDB.match(`dist/${fetchPath}`), `dist/${fetchPath}`)
-//     // event.respondWith(
-//     //   cacheDB.match(`dist/${fetchPath}`)
-//     //     .then(function (cache) {
-//     //         return cache || fetch(event.request);
-//     //     }).catch(function (err) {
-//     //         console.log(err);
-//     //         return fetch(event.request);
-//     //     })
-//     // )
-//   }
-// })
-
 self.addEventListener('fetch', async function (event) {
   const request = event.request;
   const scope = self.registration.scope;
   const url = request.url;
   const fetchPath = url.slice(scope?.length ?? 0);
 
-  console.log('Fetch request for:', fetchPath)
-  if (fetchPath === 'intercept/status') {
+  console.log('Fetch request for:')
+  if (!fetchPath) {
+      logger.log('enter fetchPath')
+      event.respondWith(self.fetch('/index.html'));
+  } else if (fetchPath === 'intercept/status') {
     event.respondWith(new Response('', { status: 234, statusText: 'intercepting' }))
   } else {
-    event.respondWith(dbResHandler(fetchPath, event))
+    // event.respondWith(dbResHandler(fetchPath, event))
+    event.respondWith(handleFetch(fetchPath, event))
   }
 })
 
-async function handlerFetch(fetchUrl: string, event): Promise<Response> {
-  logger.info(`current request blobUrl: ${fetchUrl}`)
-  const res = await fetch(fetchUrl);
-  const response = new Response(res.body, res);
-  console.log(res);
+async function handleFetch(fetchUrl: string, event): Promise<Response> {
+  const currentFileBlob: string = await localforage.getItem(`${staticPrefix}/${fetchUrl}`)
+  // logger.info(`current storage cache keys: ${await localforage.keys()}`);
+  logger.info(`current request blobUrl: ${currentFileBlob}`, `${staticPrefix}/${fetchUrl}`)
+
+  if (!currentFileBlob) {
+    console.log(event.request)
+    return fetch(event.request)
+  }
+  const res = await fetch(currentFileBlob);
   return res;
 }
 
